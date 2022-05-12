7c01d2135620a421a19281fb56316e77f855ac4a
Forat Zoabi <foratzoabi@gmail.com>
2022-05-12 08:13:36 +0200

    - added local db for offline development - created events service with a call to the get events endpoint

---
+++ events-app/src/app/services/events/event.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Event } from 'src/app/shared/models/event.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+
+  private server_url: string = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getEvents(): Observable<Event[]> {
+    return this.http.get<Event[]>(`${this.server_url}/events/uk/london`);
+  }
+
+}
